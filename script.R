8a3d12180a9d83a3b75e6d0a7788b729de002f18
rtingley <reid.tingley@gmail.com>
2015-06-30 14:35:09 +1000

    added stuff

--- script.R
+++ script.R
@@ -3,6 +3,7 @@ library(ProjectTemplate)
 #creates directories see http://projecttemplate.net/architecture.html
 create.project(".",merge.strategy = 'allow.non.conflict')
 
+# in shell:
 # should only ever have to do this once
 # in git tab - cog - shell type:
 git -config --global user.name "rtingley"
@@ -13,4 +14,7 @@ system("
        ln -s ~/FOLDER etc
        ")
 
-blah
+# in shell:
+#to get back to previous version; MAGIC NUMBER = SHA UNDER HISTORY
+git checkout MAGIC NUMBER * # is for all files, or just name file
+  
